be31112d0bf3a97493ee8acd32a9007337c1426a
Matthew Scott <matt@11craft.com>
2012-04-19 21:45:25 -0500

    fosmc-lint: Exit with 1 if lint found

--- fosmc/lint.py
+++ fosmc/lint.py
@@ -9,13 +9,15 @@ def main():
         sys.exit(1)
     path = sys.argv[1]
     db = load_data(path)
+    found_lint = False
     for data_type, objects in db.iteritems():
         for slug, obj in objects.iteritems():
             for key, value in obj.iteritems():
                 if key.startswith('lint_'):
                     lint_type = key[5:]
                     print 'type={data_type} slug={slug} lint={lint_type}'.format(**locals())
-    sys.exit(0)
+                    found_lint = True
+    sys.exit(1 if found_lint else 0)
 
 
 if __name__ == '__main__':
